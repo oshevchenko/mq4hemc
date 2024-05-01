a0e474f2df7a09ee47701951b7e98ffcb9fd691a
Oleksandr Shevchenko <oleksandrs@sapling-inc.com>
2024-05-01 14:45:45 +0300

    Add a delay to the message processing.
    Emulate real life message processing.
    
    Signed-off-by: Oleksandr Shevchenko <oleksandrs@sapling-inc.com>

--- tests/test_real_use.py
+++ tests/test_real_use.py
@@ -2,6 +2,17 @@ import threading
 from mq4hemc import HemcQueue, HemcQueueSender, HemcMessage
 from dataclasses import dataclass, field
 import logging
+import time
+
+"""
+To run this test, run the following commands:
+
+python3 -m venv ./venv
+source ./venv/bin/activate
+python3 -m pip install .
+python3 ./tests/test_real_use.py
+
+"""
 
 @dataclass
 class BigHemcMessage(HemcMessage):
@@ -22,8 +33,12 @@ class TestService(threading.Thread):
         self.get_sender().send(message)
 
     def process_item(self, item: HemcMessage):
+        if item.type == "stop":
+            return
         if hasattr(item, 'payload') and item.payload is not None:
-            print(f"Processing message '{item.type}', payload: {item.payload}")
+            # Simulate processing time
+            time.sleep(1)
+        print(f"Processed message '{item.type}', payload: {item.payload}")
         return item.type
 
     def __init__(self):
@@ -50,8 +65,24 @@ if __name__ == "__main__":
     message.type = "test1"
     message.payload = {"key": "value"}
     # the return value of send_wait_reply() is the return value of HemcQueue.process_item_cb()
+    print(f"Send {message} and do not wait for reply.")
+    status = service.get_sender().send(message)
+    message = BigHemcMessage()
+    message.type = "test2"
+    message.payload = {"key": "value"}
+    print(f"Send {message} and do not wait for reply.")
+    status = service.get_sender().send(message)
+    message = BigHemcMessage()
+    message.type = "test3"
+    message.payload = {"key": "value"}
+    print(f"Send {message} and do not wait for reply.")
+    status = service.get_sender().send(message)
+    message = BigHemcMessage()
+    message.type = "test4"
+    message.payload = {"key": "value"}
+    print(f"Now send {message} and wait for reply...")
     status = service.get_sender().send_wait_reply(message)
-    print(f"Status: {status}")
+    print(f"Reply: {status}")
     service.stop()
     service.join()
 
