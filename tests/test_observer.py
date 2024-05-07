b8cacaf9cfd53b4e65eec7fb4f69c17237b3975e
Oleksandr Shevchenko <shevchenko.adb@gmail.com>
2024-05-07 07:44:21 +0300

    HemsObserver saves the callbacks return values.

--- tests/test_observer.py
+++ tests/test_observer.py
@@ -21,29 +21,34 @@ To install the package locally, run the following command:
 make install
 """
 
+
 class GNSSObserver(HemcObserver):
     pass
+
 
 class GNSSObserverEvent(HemcObserverEvent, observer_class=GNSSObserver):
     pass
 
+
 class AnotherObserver(HemcObserver):
     pass
+
 
 class AnotherObserverEvent(HemcObserverEvent, observer_class=AnotherObserver):
     pass
 
+
 class TestObserverEventM(unittest.TestCase):
     def test_init(self):
+        GNSSObserver.clear()
+
         mock_process_message = Mock()
         mock_process_message.return_value = "success"
 
         gnss_observer = GNSSObserver()
         gnss_observer.observe("test_id", mock_process_message)
 
-        msg = HemcMessage()
-        msg.type = "test_id"
-        GNSSObserverEvent(msg)
+        GNSSObserverEvent(HemcMessage(type="test_id"))
         # assert mock_process_message.called_once_with(msg_id, msg_data)
         print(f"gnss_observer._observers: {gnss_observer._observers}")
         mock_process_message1 = Mock()
@@ -51,10 +56,8 @@ class TestObserverEventM(unittest.TestCase):
 
         another_observer = GNSSObserver()
         another_observer.observe("test_id", mock_process_message1)
-        msg = HemcMessage()
-        msg.type = "test_id"
-        GNSSObserverEvent(msg)
 
+        GNSSObserverEvent(HemcMessage(type="test_id"))
         assert 2 == mock_process_message.call_count
         for call in mock_process_message.call_args_list:
             args, kwargs = call
@@ -67,7 +70,34 @@ class TestObserverEventM(unittest.TestCase):
         print(f"gnss_observer._observers: {gnss_observer._observers}")
         print(f"another_observer._observers: {another_observer._observers}")
 
+    def test_ret(self):
+        GNSSObserver.clear()
 
+        mock_process_message = Mock()
+        mock_process_message.return_value = "success"
+
+        gnss_observer = GNSSObserver(name="gnss_observer")
+        gnss_observer.observe("test_id", mock_process_message)
+
+        ret_dict = {}
+        GNSSObserverEvent(HemcMessage(type="test_id"), ret_dict=ret_dict)
+        assert ret_dict == {"gnss_observer": "success"}
+        # assert mock_process_message.called_once_with(msg_id, msg_data)
+        mock_process_message1 = Mock()
+        mock_process_message1.return_value = "success1"
+        another_observer = GNSSObserver(name="gnss_observer1")
+        another_observer.observe("test_id", mock_process_message1)
+
+        ret_dict.clear()
+        GNSSObserverEvent(HemcMessage(type="test_id"), ret_dict=ret_dict)
+        print(f"ret_dict: {ret_dict}")
+        assert ret_dict == {"gnss_observer": "success", "gnss_observer1": "success1"}
+        assert 2 == mock_process_message.call_count
+        for call in mock_process_message.call_args_list:
+            args, kwargs = call
+            print(f"Args: {args}, Kwargs: {kwargs}")
+            assert args == (HemcMessage(type='test_id', callback=None),)
+        assert mock_process_message1.called_once_with((HemcMessage(type='test_id', callback=None),))
         # observer_class = MagicMock()
         # observer_class._mutex = MagicMock()
         # observer_class._observers = []
@@ -86,5 +116,27 @@ class TestObserverEventM(unittest.TestCase):
         original[0][0] = 99
 
         print(shallow_copy)
+
+    def test_fire1(self):
+        import copy
+
+        original = [[1, 2, 3], [4, 5, 6]]
+        shallow_copy = copy.copy(original)
+
+        shallow_copy[0][0] = 99
+
+        print(original)
+
+    def test_fire2(self):
+        def deep_copy_ret(data_dict):
+            local_dict = {"key": "value"}
+            data_dict.update(local_dict)
+
+        original = {}
+        deep_copy_ret(original)
+
+        print(f"return after deepcopy {original}")
+
+
 if __name__ == "__main__":
     unittest.main()
